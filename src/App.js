95b6aa2e2c31c897dc661cb0383ef4c68be21fcb
MOHAMED BAKULY <119895383+MOHAMEDBAKULY@users.noreply.github.com>
2024-06-04 10:51:48 +0300

    FlashCards game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Flashcard />
     </div>
   );
 }
 
-export default App;
+const cards = [
+  {
+    id: 1,
+    question: "What is the Smallest counrty in the world",
+    answer: "Vatican City",
+  },
+  {
+    id: 2,
+    question: "Tallest mountain in Afica",
+    answer: "Kilimanjaro",
+  },
+  {
+    id: 3,
+    question: "Biggest aircraft ever made",
+    answer: "Antonov An-225 Mriya",
+  },
+  {
+    id: 4,
+    question: "What is photohive",
+    answer: "Career platform for artist",
+  },
+  {
+    id: 5,
+    question: "Biggest dream I chase everyday",
+    answer: "Build Photohive",
+  },
+  {
+    id: 6,
+    question: "I will buy the domain next month",
+    answer: "photohive.io",
+  },
+];
+
+function Header() {
+  return <header>Simple FlashCards layout</header>;
+}
+
+function Flashcard() {
+  const [selectedId, setSelectedId] = useState(null);
+
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : null);
+  }
+
+  return (
+    <div>
+      <div className="cards">
+        {cards.map((card) => (
+          <div
+            key={card.id}
+            onClick={() => handleClick(card.id)}
+            className={card.id === selectedId ? "selected" : ""}
+          >
+            {card.id === selectedId ? card.answer : card.question}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
